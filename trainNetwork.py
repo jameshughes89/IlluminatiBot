c98f83439aea0925538f4bfbff6f53a490d1bd12
James Hughes <jameshughes89@gmail.com>
2017-07-17 16:18:21 -0400

    minor comments added to network training script

--- trainNetwork.py
+++ trainNetwork.py
@@ -1,6 +1,13 @@
 '''
 Script to train the ANN for finding triangles. 
 Will be a convolutional NN.
+
+TODO:
+- Build conv network
+- Get training working right
+- Make work on arbitrary size images
+- Save network to be loaded in later
+
 '''
 
 import tensorflow
